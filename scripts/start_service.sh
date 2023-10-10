b368f4c1d4ab78f2e709066270a4b6e1a407cc6b
jp34 <joekadlic@outlook.com>
2023-10-10 01:18:27 -0400

    Updater .gitignore & .dockerignore

--- scripts/start_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d \
-    --name sn-service-tags \
-    --network sn \
-    -p 8000:8000 \
-    --env-file .env \
-    socialnet/sn-service-tags
--- scripts/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker kill sn-service-tags
-sudo docker rm sn-service-tags
-sudo docker kill sn-db-tags
-sudo docker rm sn-db-tags
